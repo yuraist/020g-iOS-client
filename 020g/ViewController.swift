390d35ed36b448b74bcdea51a0c502d9fcc7af29
Yuraist <yura.ist@icloud.com>
2018-09-17 11:48:23 +0300

    Initial Commit

---
+++ 020g/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  020g
+//
+//  Created by Юрий Истомин on 17/09/2018.
+//  Copyright © 2018 Юрий Истомин. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
