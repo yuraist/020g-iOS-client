8fcc4e4f7df0c3a1d506b380595b92384896d629
Yuraist <yura.ist@icloud.com>
2018-09-24 10:16:32 +0300

    Implemented a slide from left side menu

--- 020g/Controllers/ContainerViewController.swift
+++ 020g/Controllers/ContainerViewController.swift
@@ -14,11 +14,18 @@ class ContainerViewController: UIViewController {
     return .lightContent
   }
   
-  var isShowMenu = false
+  var isShowMenu = false {
+    didSet {
+      showShadowForCenterViewController(shouldShowShadow: isShowMenu)
+    }
+  }
+  
+  let centerPanelExpandedOffset: CGFloat = 100
   
   // Add additional controllers
   var centerNavigationController: UINavigationController!
   var mainController: MainTableViewController!
+  var menuViewController: MenuViewController?
   
   override func viewDidLoad() {
     super.viewDidLoad()
@@ -56,14 +63,57 @@ class ContainerViewController: UIViewController {
 
 extension ContainerViewController: CenterViewControllerDelegate {
   func toggleLeftPanel() {
-    show(MenuViewController(), sender: self)
+    if !isShowMenu {
+      addLeftPanelViewController()
+    }
+    
+    animateLeftPanel(shouldExpand: !isShowMenu)
   }
   
+  // Creates a menuViewController instance and adds it to the viewController
   func addLeftPanelViewController() {
+    guard menuViewController == nil else { return }
     
+    let vc = MenuViewController()
+    addChild(sidePanelController: vc)
+    menuViewController = vc
+  }
+  
+  // Adds the side panel controller's view below the container controller's view
+  func addChild(sidePanelController: MenuViewController) {
+    view.insertSubview(sidePanelController.view, at: 0)
+    addChild(sidePanelController)
+    sidePanelController.didMove(toParent: self)
   }
   
   func animateLeftPanel(shouldExpand: Bool) {
-    
+    if shouldExpand {
+      isShowMenu = !isShowMenu
+      animateCenterPanelXPosition(targetPosition: centerNavigationController.view.frame.width - centerPanelExpandedOffset)
+    } else {
+      animateCenterPanelXPosition(targetPosition: 0) { finished in
+        self.isShowMenu = !self.isShowMenu
+        self.menuViewController?.view.removeFromSuperview()
+        self.menuViewController = nil
+      }
+    }
+  }
+  
+  func animateCenterPanelXPosition(targetPosition: CGFloat, completion: ((Bool) -> Void)? = nil) {
+    UIView.animate(withDuration: 0.5,
+                   delay: 0,
+                   usingSpringWithDamping: 0.8,
+                   initialSpringVelocity: 0,
+                   options: .curveEaseInOut, animations: {
+                    self.centerNavigationController.view.frame.origin.x = targetPosition
+    }, completion: completion)
+  }
+  
+  func showShadowForCenterViewController(shouldShowShadow: Bool) {
+    if shouldShowShadow {
+      centerNavigationController.view.layer.shadowOpacity = 0.8
+    } else {
+      centerNavigationController.view.layer.shadowOpacity = 0.0
+    }
   }
 }
