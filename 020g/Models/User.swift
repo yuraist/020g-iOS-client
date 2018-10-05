176fff6087e4efc6dbb04614b34ca8d8a8620895
Yuraist <yura.ist@icloud.com>
2018-10-05 08:30:07 +0300

    Created User model

---
+++ 020g/Models/User.swift
@@ -0,0 +1,16 @@
+//
+//  User.swift
+//  020g
+//
+//  Created by Юрий Истомин on 05/10/2018.
+//  Copyright © 2018 Юрий Истомин. All rights reserved.
+//
+
+import UIKit
+
+struct User: Codable {
+  var name: String
+  var phone: String
+  var email: String?
+  var pay: String
+}
