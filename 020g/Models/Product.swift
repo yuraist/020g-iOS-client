1348aec5a03cde1aa696edecad504904d8cf0d32
Yuraist <yura.ist@icloud.com>
2018-10-08 12:58:47 +0300

    Implemented full product model and the ProductResponse model

--- 020g/Models/Product.swift
+++ 020g/Models/Product.swift
@@ -5,6 +5,8 @@
 //  Created by Юрий Истомин on 27/09/2018.
 //  Copyright © 2018 Юрий Истомин. All rights reserved.
 //
+// * I'm sorry for properties that are called "aviable", but it's not my mistake.
+// The name I get from API, so it's more clearly and comfortably to use this name here.
 
 import UIKit
 
@@ -49,4 +51,57 @@ struct Products: Codable {
   enum CodingKeys: String, CodingKey {
     case list = "list"
   }
+}
+
+struct ProductFull: Codable {
+  var id: Int
+  var costs: String
+  var cost: Int
+  var name: String
+  var desc: String
+  var images: [String]
+  var opts: [Option]
+  var prices: [Price]
+}
+
+struct Option: Codable {
+  var name: String
+  var fil: String?
+  var value: String
+}
+
+struct Price: Codable {
+  var id: Int
+  var site: String
+  var aviable: Bool
+  var capt: String
+  var city: String
+  var price: String
+}
+
+struct Breadcrumb: Codable {
+  var id: Int
+  var name: String
+}
+
+struct Breadcrumbs: Codable {
+  var breadcrumbs: [Breadcrumb]
+}
+
+struct City: Codable {
+  var name: String
+  var count: Int
+  var selected: Bool
+  var aviable: Bool
+}
+
+struct Cities: Codable {
+  var cities: [City]
+}
+
+struct ProductResponse: Codable {
+  var status: Bool
+  var product: Product
+  var breadcrumbs: Breadcrumbs
+  var cities: Cities
 }
