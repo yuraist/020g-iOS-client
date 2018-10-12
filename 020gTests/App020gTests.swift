934de0f3fb17b5945bdb796fa7056ac01a26c1c6
Yuraist <yura.ist@icloud.com>
2018-10-12 09:33:01 +0300

    Renamed project

---
+++ 020gTests/App020gTests.swift
@@ -0,0 +1,27 @@
+//
+//  App020gTests.swift
+//  020gTests
+//
+//  Created by Юрий Истомин on 12/10/2018.
+//  Copyright © 2018 Юрий Истомин. All rights reserved.
+//
+
+import XCTest
+@testable import App020g
+
+
+class App020gTests: XCTestCase {
+  
+  override func setUp() {
+    
+  }
+  
+  override func tearDown() {
+    
+  }
+  
+  func testExample() {
+    
+  }
+  
+}
--- Podfile
+++ Podfile
@@ -1,10 +1,10 @@
 # Uncomment the next line to define a global platform for your project
 # platform :ios, '9.0'
 
-target '020g' do
+target 'App020g' do
   # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
   use_frameworks!
 
-  # Pods for 020g
+  # Pods for App020g
   pod 'Kingfisher', '~> 4.0'
 end
