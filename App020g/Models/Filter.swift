cf0c391318e3112d3e4132bf1a86ca66b08fdfcc
Yuraist <yura.ist@icloud.com>
2018-10-23 14:30:55 +0300

    Implemented the Filter model

---
+++ App020g/Models/Filter.swift
@@ -0,0 +1,33 @@
+//
+//  Filter.swift
+//  App020g
+//
+//  Created by Юрий Истомин on 23/10/2018.
+//  Copyright © 2018 Юрий Истомин. All rights reserved.
+//
+
+import Foundation
+
+struct FilterResponse: Codable {
+  var status: Bool
+  var count: Int
+  var list: [FilterParameter]
+}
+
+struct FilterParameter: Codable {
+  var id: Int
+  var name: String
+  var type: Int
+  var options: [FilterOption]?
+  var cost_min_orig: Int?
+  var cost_max_orig: Int?
+  var cost_min: Int?
+  var cost_max: Int?
+}
+
+struct FilterOption: Codable {
+  var name: String
+  var value: Int
+  var count: Int
+  var selected: Bool
+}
